3027e1e81eafc69b2b3895619a1324a8fd129115
Bertrand MARIE <photomb09@gmail.com>
2024-09-18 12:08:24 +0200

    model upgrade

--- models/surfspots_model.go
+++ models/surfspots_model.go
@@ -8,5 +8,11 @@ type SurfSpots struct {
 	Latitude  	float64 `json:"latitude"`
 	Longitude 	float64 `json:"longitude"`
 	Risk 		string 	`json:"risk"`
+	Weather 	string 	`json:"weather"`
+	Temp 		int8 	`json:"temp"`
+	Tide 		string 	`json:"tide"`
+	Coeff 		int8 	`json:"coeff"`
+	Swellsea 	string 	`json:"swell_sea"`
+	Wind 		string 	`json:"wind"`
 	Description string 	`json:"description"`
 }
